51e01d5affc7099ddf9f5882c84630c6ba5ac43f
furkanevin <furkanevin0@gmail.com>
2023-07-08 01:53:41 +0300

    dynamic listing and addModal

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-// HTML'den gelenler
+//! importlar (diğer js dosyasında gelen değişken ve fonksiyonlar)
+import { months } from './constants.js';
+import { renderMails, showModal } from './ui.js';
+
+// localstorage'dan veri alma
+const strMailData = localStorage.getItem('data');
+// gelen string veriyi obje ve dizileri çevirme
+const mailData = JSON.parse(strMailData);
+
+//! HTML'den gelenler
 const hamburgerMenu = document.querySelector('.menu');
 const navigation = document.querySelector('nav');
+const mailsArea = document.querySelector('.mails-area');
+const createMailBtn = document.querySelector('.create-mail');
+const closeMailBtn = document.querySelector('#close-btn');
+const modal = document.querySelector('.modal-wrapper');
+const form = document.querySelector('#create-mail-form');
 
-// Olay İzleyicleri
+//! Olay İzleyicleri
+// ekranın yüklnme anında çalışır
+document.addEventListener('DOMContentLoaded', () =>
+  renderMails(mailsArea, mailData)
+);
 hamburgerMenu.addEventListener('click', handleMenu);
+// modal işlemleri
+createMailBtn.addEventListener('click', () => showModal(modal, true));
+closeMailBtn.addEventListener('click', () => showModal(modal, false));
+form.addEventListener('submit', sendMail);
 
+//! Fonksiyonlar
 // Navigasyonu açıp kapamaya yarayan fonksiyon
 // Hamburger menüsüne tıklanınca çalışır
 function handleMenu() {
@@ -16,21 +39,57 @@ function handleMenu() {
   navigation.classList.toggle('hide');
 }
 
-function topla(a, b) {
-  const toplam = a + b;
-
-  let cıkarma = a - b - a;
-
-  let metin2 = 'asdasd';
-  // eğerki fonksiyonun sonucunu farklı
-  // bir yerde kullanmak istiyorsak
-  // o zaman return satı ile sonucu göndermemiz
-  return toplam;
+// tarih oluşturan fonksiyon
+function getDate() {
+  // bugünün tarihini alma
+  const dateArr = new Date().toLocaleDateString().split('/');
+  // tarih dizisinden günü alma
+  const day = dateArr[0];
+  // tarih diizisnde kaçıncı ayda olduğumuz bilgisini alma
+  const monthNumber = dateArr[1];
+  // ayın sırasına karşılık gelen ismi tanımladık
+  const month = months[monthNumber - 1];
+  // fonksiyonun çağrıldığı yere gönderilcek cevap
+  return day + ' ' + month;
 }
 
-const metin = `Bugün 2 farklı vardiyada toplam ${topla(
-  10,
-  5
-)} saat çalıştım`;
+function sendMail(e) {
+  // sayfanın yenilenmesini engelleme
+  e.preventDefault();
 
-console.log(metin);
+  // formun içerisinde yer alan inputların
+  // değerlerine erişme
+  const receiver = e.target[0].value;
+  const title = e.target[1].value;
+  const message = e.target[2].value;
+
+  // yeni mail objesi oluşturma
+  const newMail = {
+    id: new Date().getTime(),
+    sender: 'Furkan',
+    receiver,
+    title,
+    message,
+    date: getDate(),
+  };
+
+  // oluştudupğumuz objeyi dizinin başına ekleme
+  mailData.unshift(newMail);
+
+  //Todo veritabanını(localstorage) güncelle
+  // storage'a gönderimek için string'e çeviriyoruz
+  const strData = JSON.stringify(mailData);
+  // storeage'a gönderme
+  localStorage.setItem('data', strData);
+
+  // ekranı güncelle
+  renderMails(mailsArea, mailData);
+
+  // modalı kapat
+  showModal(modal, false);
+
+  // modal'ı temizle
+  e.target[0].value = ' ';
+  e.target[1].value = ' ';
+  e.target[2].value = ' ';
+}
---
+++ ui.js
@@ -0,0 +1,51 @@
+// yazıları kesemk için kullandığımız fonk.
+function trimString(str, max) {
+  // metin 50 karakterden kısaysa olduğu gibi gönderiyorux
+  if (str.length < max) return str;
+
+  // metnin harf uzunluğu 50 karakterden uzunsa
+  // 50 ye kadar olan kısmı kes sonrasına ... koy
+  // yeni metni fonksiyonun çalıştırıldığı yere gönder
+  return str.slice(0, max) + '...';
+}
+
+/*
+ * Ekrana mailleri listeleyecek fonksiyon
+ * Outlet: ekranın hangi kısmına müdahale edilcek
+ * Data: Hangi verileri ekrana basıcaz
+ */
+export function renderMails(outlet, data) {
+  if (!data) return;
+  // herbir mail objesi için bir maili temsil eden html oluştur
+  // oluşan mail html'ini Mailler alanına gönder (ekran bas)
+  outlet.innerHTML = data
+    .map(
+      (mail) => `
+         <div class="mail">
+            <div class="left">
+              <input type="checkbox" />
+              <i class="bi bi-star"></i>
+              <span>${mail.sender}</span>
+            </div>
+            <div class="right">
+              <p class="message-title">${trimString(
+                mail.title,
+                30
+              )}</p>
+              <p class="message-desc">
+                ${trimString(mail.message, 40)}
+              </p>
+              <p class="message-date">${mail.date}</p>
+
+              <button id="delete">Sil</button>
+            </div>
+          </div>
+  `
+    )
+    .join(' ');
+}
+
+// ekrana mail oluşturma penceri açıcak fonk.
+export function showModal(modal, willOpen) {
+  modal.style.display = willOpen ? 'grid' : 'none';
+}
